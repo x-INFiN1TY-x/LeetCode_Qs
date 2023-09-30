6e4f9ecb65bfb61dbec51cb4b35df9b7643eb37e
x-INFiN1TY-x <111151372+x-INFiN1TY-x@users.noreply.github.com>
2023-09-30 14:11:42 +0530

    Time: 3 ms (42.62%), Space: 6.6 MB (7.58%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,24 +1,34 @@
-class Solution {
-public:
-    double myPow(double x, int n) {
-        double ans = 1;
-        long long m = n;
-        if(n < 0){
-            m = (-1) * m;
-        }
-        while(m > 0){
-            if(m % 2 == 1){
-                ans = ans * x;
-                m = m -1;
+class Solution
+{
+    public:
+        double myPow(double x, int n)
+        {
+            ios_base::sync_with_stdio(false);
+            cin.tie(nullptr);
+            if (!n)
+                return 1.00000;
+            else
+            {
+                double ans = 1;
+                long long m = n;
+                if (m < 0)
+                    m *= -1;
+                while (m)
+                {
+                    if (m % 2 == 1)
+                    {
+                        ans *= x;
+                        m--;
+                    }
+                    else
+                    {
+                        x *= x;
+                        m /= 2;
+                    }
+                }
+                if (n < 0)
+                    ans = 1 / ans;
+                return ans;
             }
-            else{
-                x = x*x;
-                m = m/2;
-            }
         }
-        if(n < 0){
-            ans = 1/ans;
-        }
-        return ans;
-    }
 };
