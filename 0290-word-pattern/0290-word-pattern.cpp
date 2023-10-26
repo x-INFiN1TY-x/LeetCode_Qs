86a25be8ebf426225055233461993843e1310fe8
x-INFiN1TY-x <111151372+x-INFiN1TY-x@users.noreply.github.com>
2023-10-26 19:33:54 +0530

    Time: 3 ms (42.55%), Space: 6.8 MB (28.01%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,46 +1,35 @@
-class Solution
-{
-    public:
-        bool wordPattern(string pattern, string s)
-        {
-            vector<string> v;
-           	// word variable to store word in the string s
-            string word;
-           	// making a string stream
-            stringstream iss(s);
-           	// Push each word in vector
-            while (iss >> word)
-                v.push_back(word);
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        int n = pattern.size();
+        stringstream iss(s);
+        string word;
+        vector<string> line;
 
-            set<string> S;	// To check if the same word is not mappped with another character
-           	// If vector and pattern are of different sizes there can be no mapping
-            if (pattern.size() != v.size())
-                return false;
-            unordered_map<char, string> m;
-            for (int i = 0; i < pattern.size(); i++)
-            {
-               	// Take current character in pattern
-                char ch = pattern[i];
-               	// If current character is present in the map
-                if (m.find(ch) != m.end())
-                {
-                   	//Check if the string mapped to that character is different or not if yes
-                   	//return false
-                    if (m[ch] != v[i])
-                        return false;
-                }
-               	// If not present in the map
-                else
-                {
-                   	// Check if the string is previously in set
-                   	// If it is in set it means it is already mapped return false
-                    if (S.count(v[i]) > 0)
-                        return false;
-                   	// Other wise insert into map and set
-                    m[ch] = v[i];
-                    S.insert(v[i]);
+        while (iss >> word) {
+            line.push_back(word);
+        }
+
+        if (n != line.size()) {
+            return false;
+        }
+
+        unordered_map<char, string> charToWord;
+        unordered_map<string, char> wordToChar;
+
+        for (int i = 0; i < n; i++) {
+            char ch = pattern[i];
+            string w = line[i];
+
+            if (charToWord.find(ch) == charToWord.end() && wordToChar.find(w) == wordToChar.end()) {
+                charToWord[ch] = w;
+                wordToChar[w] = ch;
+            } else {
+                if (charToWord[ch] != w || wordToChar[w] != ch) {
+                    return false;
                 }
             }
-            return true;
         }
+        return true;
+    }
 };
