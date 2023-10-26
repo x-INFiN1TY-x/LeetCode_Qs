d786ae68abdd51e1cfdae1fa2923dd4855670dbf
x-INFiN1TY-x <111151372+x-INFiN1TY-x@users.noreply.github.com>
2023-10-26 19:39:27 +0530

    Time: 0 ms (100%), Space: 7.1 MB (9.02%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,21 +1,17 @@
-class Solution {
-public:
-    /*
-    Approach: 
-    Traverse from end to the first whitespace character and count the number of letters.
-    Return the count as our pointer hits the whitespace character.
-    
-    Time complexity: O(n)
-    Space complexity: O(1)
-    */    
-    int lengthOfLastWord(string s) {
-        int n = s.length();
-        
-        int ptr = n-1;
-        while(ptr >= 0 && s[ptr] == ' ') ptr--; /* Skip the trailing whitespaces */
-        
-        int len = 0;
-        while(ptr >= 0 && s[ptr--] != ' ') len++; /* Counting the letters in the last word */
-        return len;
-    }
+class Solution
+{
+    public:
+        int lengthOfLastWord(string s)
+        {
+            stringstream line(s);
+            string word;
+            while (!line.eof())
+            {
+                line >> word;
+            }
+            if (!word.empty())
+                return word.size();
+            else
+                return 0;
+        }
 };
