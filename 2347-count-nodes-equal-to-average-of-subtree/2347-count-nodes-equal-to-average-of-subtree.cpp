33a7a572a08980341b3922b46f64ea44411617e7
x-INFiN1TY-x <111151372+x-INFiN1TY-x@users.noreply.github.com>
2023-11-02 16:09:57 +0530

    Time: 0 ms (100%), Space: 12.3 MB (54.24%) - LeetHub

--- 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.cpp
@@ -1,40 +1,48 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    int averageOfSubtree(TreeNode* root) {
-        return helper(root).numEqual; 
-    }
-    
-private:
-    struct Info {
-        int sum;
-        int numNodes;
-        int numEqual;
-    };
-    
-    Info helper(TreeNode* node) {
-        if (!node) {
-            return {0, 0, 0};
+class Solution
+{
+    public:
+        int averageOfSubtree(TreeNode *root)
+        {
+            return helper(root).numEqual;
         }
-        
-        auto left = helper(node->left);
-        auto right = helper(node->right);
-        
-        int sum = left.sum + right.sum + node->val;
-        int numNodes = left.numNodes + right.numNodes + 1;
-        int avg = sum / numNodes;
-        int numEqual = (node->val == avg) + left.numEqual + right.numEqual;
-        
-        return {sum, numNodes, numEqual};
+
+    private:
+        struct Info
+        {
+            int sum;
+            int numNodes;
+            int numEqual;
+        };
+
+    Info helper(TreeNode *node)
+    {
+        if (!node)
+        {
+            return { 0,
+                0,
+                0 };
+        }
+
+        Info left = helper(node->left);
+        Info right = helper(node->right);
+
+        int numEqual = (node->val == ((left.sum + right.sum + node->val) / (left.numNodes + right.numNodes + 1))) + left.numEqual + right.numEqual;
+
+        return {
+            left.sum + right.sum + node->val,
+            left.numNodes + right.numNodes + 1,
+            numEqual
+        };
     }
 };
