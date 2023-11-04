8c030eed890091a31eec4793124a41898863b168
x-INFiN1TY-x <111151372+x-INFiN1TY-x@users.noreply.github.com>
2023-11-04 19:08:50 +0530

    Time: 19 ms (48.28%), Space: 23.2 MB (14.66%) - LeetHub

---
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/1627-last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+    public:
+        int getLastMoment(int n, std::vector<int> &left, std::vector<int> &right)
+        {
+
+            int maxLeft = left.empty() ? 0 : *std::max_element(left.begin(), left.end());
+
+            int minRight = right.empty() ? n : *std::min_element(right.begin(), right.end());
+
+            return std::max(maxLeft, n - minRight);
+        }
+};
