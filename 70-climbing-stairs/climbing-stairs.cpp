9bae989b08b8191d7ec3fcae8d3e0f7396f5bea9
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-01-28 19:57:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (5.72%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -5,23 +5,19 @@ auto fastio =[]()
     cout.tie(nullptr);
     return nullptr;
 }();
-vector<int> arr(46, -1);
 class Solution
 {
     public:
     int climbStairs(int n)
     {
-       	// base
-        if (n <= 0)
-            return 0;
-        else if (n == 1 || n == 2 || n == 3)
-            return n;
-       	// mem
-        if (arr.at(n) != -1)
-            return arr.at(n);
-        else
-            arr[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        vector<int> arr(n+1);
+        arr[0]=1;
+        arr[1]=1;
+        for(int i =2;i<=n;i++)
+        {
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
 
-        return arr.at(n);
     }
 };
