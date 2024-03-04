37fe55f89aae0e8a368fb99e17362795ff3166ec
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-03-05 03:55:18 +0530

    Time: 4 ms (75.27%) | Memory: 13.1 MB (24.03%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.cpp
@@ -0,0 +1,25 @@
+#pragma GCC optimize("O3", "unroll-loops")
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        if (tokens.empty()) return 0;//edge case
+        sort(begin(tokens),end(tokens));
+        if(power<tokens[0]) return 0;
+        int l=0, r=tokens.size()-1;
+        int score=0;
+        while(l<=r){// If power is infinity l can be r
+            while (l<=r && power>=tokens[l]){
+               power-=tokens[l];
+               l++;
+               score++;
+            }
+        //    maxScore=max(maxScore, score);
+            if (l<r && score>0) { 
+                power+= tokens[r--];
+                score--;
+            }
+            else break;
+        }
+        return score; 
+    }
+};
