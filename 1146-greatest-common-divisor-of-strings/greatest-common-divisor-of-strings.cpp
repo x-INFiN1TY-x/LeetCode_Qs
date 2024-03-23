b25d988de884e7b0d2d321e239b1d985b41e41b2
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-03-23 22:44:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (27.89%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -6,18 +6,9 @@ public:
         if (str1 + str2 != str2 + str1)
             return "";
         
-        int len1 = str1.length();
-        int len2 = str2.length();
-        int gcd = 1;
+        int gd = gcd(str1.length(),str2.length());
         
-        // Calculating gcd
-        while (len2 != 0) {
-            int temp = len2;
-            len2 = len1 % len2;
-            len1 = temp;
-        }
-        gcd = len1;
         
-        return str1.substr(0, gcd);
+        return str1.substr(0, gd);
     }
 };
