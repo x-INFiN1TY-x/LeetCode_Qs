3520500d8408051bf03e6224769846fc19f52c27
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-03-23 23:30:44 +0530

    Time: 11 ms (70.32%) | Memory: 22.8 MB (53.11%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -3,38 +3,15 @@ using namespace std;
 
 class Solution {
 public:
-    bool check(vector<int>& flowerbed, int i) {
-        if (i < 0 || i >= flowerbed.size()) // Added boundary check
-            return false;
-        if ((i == 0 || flowerbed[i - 1] == 0) &&  // Fixed the condition
-            flowerbed[i] == 0 && 
-            (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        if (flowerbed.front() == 0) {
-        if (check(flowerbed, 0)) { // Fixed the function call
-                n--;
-                flowerbed.front() = 1;
-            }
-        }
-        if (flowerbed.back() == 0) {
-            if (check(flowerbed, flowerbed.size()- 1)) { // Fixed the function call
-                n--;
-                flowerbed.back() = 1;
-            }
-            }
-        for (int i = 1; i < flowerbed.size() - 1; i++) {
-            if (check(flowerbed, i)) { // Fixed the function call
-                n--;
+        int count = 0;
+        int size = flowerbed.size();
+        for (int i = 0; i < size; ++i) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == size - 1 || flowerbed[i + 1] == 0)) {
                 flowerbed[i] = 1;
+                ++count;
             }
         }
-        return n <= 0;
+        return count >= n;
     }
 };
