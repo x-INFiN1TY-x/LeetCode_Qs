2e2943776054dd9d340e4f5803ea35d53f687abe
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-03-24 20:21:10 +0530

    Time: 18 ms (98.95%) | Memory: 23.7 MB (99.98%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -1,20 +1,31 @@
-auto fastio=[]()  {
+auto fastio =[]()
+{
     std::ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     return nullptr;
 }();
 
-class Solution {
-public:
-    TreeNode* searchBST(TreeNode *root, int val) {
-        if (!root)
-            return nullptr;
-        if (root->val == val)
-            return root;
-        else if (root->val > val)
-            return searchBST(root->left, val);
-        else // root->val < val
-            return searchBST(root->right, val);
-    }
+class Solution
+{
+    public:
+        TreeNode* searchBST(TreeNode *root, int val)
+        {
+            if (!root)
+            {
+                return nullptr;
+            }
+            if (root->val == val)
+                {return root;}
+            else if (root->val > val)
+            {
+                root->right = nullptr;
+                return searchBST(root->left, val);
+            }
+            else	// root->val < val
+            {
+                root->left = nullptr;
+                return searchBST(root->right, val);
+            }
+        }
 };
