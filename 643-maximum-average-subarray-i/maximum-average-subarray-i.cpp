0732c7b47864690a30650e662bd2c7dfe7a70eae
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-03-24 17:12:17 +0530

    Time: 136 ms (21.88%) | Memory: 112.5 MB (17.84%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <algorithm> // for max_element
+#include <climits> // for INT_MIN
+
+class Solution {
+public:
+    double findMaxAverage(std::vector<int>& nums, int k) {
+        if (k == 1) {
+            return *std::max_element(nums.begin(), nums.end());
+        }
+        if (nums.size() == 1)
+            return nums.front();
+            
+        int m = 0, mx = INT_MIN;
+        for (int i = 0; i < k; i++)
+            m += nums[i];
+        if (k == nums.size())
+            return static_cast<double>(m) / k;
+
+        mx = std::max(mx, m); // Update maximum sum
+
+        for (int i = k; i < nums.size(); i++) {
+            // Update the sum of the last k elements
+            m += nums[i] - nums[i - k];
+            mx = std::max(mx, m); // Update maximum sum
+        }
+        return static_cast<double>(mx) / k;
+    }
+};
