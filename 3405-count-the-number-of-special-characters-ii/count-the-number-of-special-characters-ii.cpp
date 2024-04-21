59b84f6a051100332c50ec23691b241d8ef64e45
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-04-21 11:49:28 +0530

    Time: 61 ms (85.71%) | Memory: 24.2 MB (71.43%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -1,41 +1,23 @@
-#include <string>
-#include <unordered_map>
-
-class Solution
-{
-    public:
-        int numberOfSpecialChars(std::string word)
-        {
-            std::unordered_map<char, int> arr;
-            int count = 0;
-            for (int i = 0; i < word.size(); i++)
-            {
-                if (std::islower(word[i]))
-                {
-                    if (arr[word[i]] == INT_MIN)
-                    {
-                        count--;
-                        arr[word[i]] = INT_MAX;
-                    }
-                    else if (arr[word[i]] == INT_MAX)
-                        continue;
-                    else
-                        arr[word[i]]++;
-                }
-                if (std::isupper(word[i]))
-                {
-                    arr[word[i]]++;
-                    if (arr[word[i] + 32] == INT_MAX || arr[word[i] + 32] == 0)
-                    {
-                        continue;
-                    }
-                    else if (arr[word[i] + 32] > 0 && arr[word[i]] == 1)
-                    {
-                        count++;
-                        arr[word[i] + 32] = INT_MIN;
-                    }
-                }
+class Solution {
+public:
+    int numberOfSpecialChars(string s) {
+        int n = s.length();
+        int count = 0;
+        vector<int> a(150, -1); // upperCase
+        vector<int> b(150, -1); // lowerCase
+        for(int i = 0; i < n; i++){
+            if(s[i] >= 65 && s[i] <= 90){ // count first occurrence of capital letters 
+                if(a[s[i]] == -1) a[s[i]] = i; 
             }
-            return count;
+            else{ // count last occurrence of small letters
+                b[s[i]] = i;
+            }
         }
+        // iterate on either of vector and according to the condition increase count
+        for(int i = 0; i < a.size(); i++){
+            if(a[i] == -1 || b[i + 32] == -1) continue;
+            if(b[i + 32] < a[i]) count++;
+        }
+        return count;
+    }
 };
