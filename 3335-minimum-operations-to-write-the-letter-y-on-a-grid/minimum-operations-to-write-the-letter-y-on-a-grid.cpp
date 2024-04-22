babb0f5b5c5d01cb9e3ad2dbaca9873647d64165
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-04-23 00:47:03 +0530

    Time: 54 ms (46.52%) | Memory: 41 MB (78.12%) - LeetSync

--- 3335-minimum-operations-to-write-the-letter-y-on-a-grid/minimum-operations-to-write-the-letter-y-on-a-grid.cpp
+++ 3335-minimum-operations-to-write-the-letter-y-on-a-grid/minimum-operations-to-write-the-letter-y-on-a-grid.cpp
@@ -1,3 +1,10 @@
+auto fastio=[]()  {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
 class Solution {
 public:
     int minop(vector<vector<int>>& grid, int y, int ny) {
@@ -21,12 +28,11 @@ public:
         }
         return c; // Return count of operations
     }
-    
+
     int minimumOperationsToWriteY(vector<vector<int>>& grid) {
-        return min(minop(grid, 0, 1),
-                   min(minop(grid, 0, 2),
-                       min(minop(grid, 1, 0),
-                           min(minop(grid, 1, 2),
-                               min(minop(grid, 2, 1), minop(grid, 2, 0))))));
+        return min(
+            {minop(grid, 0, 1), minop(grid, 1, 0),
+             minop(grid, 1, 2), minop(grid, 2, 1),
+             minop(grid, 2, 0), minop(grid, 0, 2)});
     }
 };
