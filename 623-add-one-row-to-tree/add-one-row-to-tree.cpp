d52ce6f975365f4815961cb729a158e20416ddb3
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-05-07 11:23:46 +0530

    Time: 14 ms (45.96%) | Memory: 24.7 MB (11.72%) - LeetSync

--- 623-add-one-row-to-tree/add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/add-one-row-to-tree.cpp
@@ -1,30 +1,40 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+auto fastio=[]()  {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
 class Solution {
 public:
-    void addRow(TreeNode* node, int val, int depth, int curDepth) {
-        if (node == nullptr) return;
-        if (curDepth == depth - 1) {
+    TreeNode* addOneRow(TreeNode *root, int val, int depth) {
+        if (!root) return nullptr;
+
+        if (depth == 1)
+            return new TreeNode(val, root, nullptr);
+
+        queue<TreeNode*> q;
+        q.push(root);
+        int c = 1;
+
+        while (!q.empty() && c < depth - 1) {
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode *node = q.front();
+                q.pop();
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            ++c;
+        }
+
+        while (!q.empty()) {
+            TreeNode *node = q.front();
+            q.pop();
             node->left = new TreeNode(val, node->left, nullptr);
             node->right = new TreeNode(val, nullptr, node->right);
-            return;
         }
-        addRow(node->left, val, depth, curDepth+1);
-        addRow(node->right, val, depth, curDepth+1);
-    }
 
-    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
-        if (depth == 1) return new TreeNode(val, root, nullptr);
-        addRow(root, val, depth, 1);
         return root;
     }
 };
