e73cf9c8ff073e4003585b88627fc671d72bfab4
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-05-08 10:34:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (17.47%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,23 +1,39 @@
-class Solution
-{
-    public:
-        int trav(int i, int j, vector<vector < int>> &memo, int ans, int n, int m)
-        {
-           	//base
-            if (i >= m || j >= n) return 0;
-            if (i == m - 1 && j == n - 1)
-                return 1;
-            if (memo[i][j] != -1)
-                return memo[i][j];
-           	//rec
-            ans += trav(i, j + 1, memo, ans, n, m) + trav(i + 1, j, memo, ans, n, m);
-            memo[i][j] = ans;
-            return memo[i][j];
-        }
-    int uniquePaths(int m, int n)
-    {
-        vector<vector < int>> memo(m, vector<int> (n, -1));
+#include <vector>
+using namespace std;
 
-        return trav(0, 0, memo, 0, n, m);
+class Solution {
+public:
+    int solve(vector<vector<int>> &grid, int i, int j) {
+        // Base case
+        if (i >= grid.size() || j >= grid[0].size() || i < 0 || j < 0)
+            return 0;
+
+        // If we reached the top-left corner, return 1
+        if (i == 0 && j == 0)
+            return 1;
+
+        // If the value is already calculated, return it
+        if (grid[i][j] != -1)
+            return grid[i][j];
+
+        // Recursive cases: Move either right or down
+        grid[i][j] = solve(grid, i - 1, j) + solve(grid, i, j - 1);
+
+        return grid[i][j];
+    }
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> grid(m, vector<int>(n, -1));
+        
+        // Initialize the first column and row to 1
+        for (int i = 0; i < m; i++) {
+            grid[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            grid[0][j] = 1;
+        }
+
+        // Call the solve function with indices of the bottom-right cell
+        return solve(grid, m - 1, n - 1);
     }
 };
