4e18777ad3e52183de92077fb0185535522de663
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-07-04 00:55:39 +0530

    Time: 9 ms (11.81%) | Memory: 14.3 MB (15.27%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,34 +1,33 @@
+auto fastio=[]()  {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
 class Solution {
 public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) 
-    {
-        //insert a pair into the map. The key is the element in the nums vectors, 
-        //the values for each pair represent the total elements of that number in each respective array
-        //then get the minimum value for each pair set in the hashmap, simples.
-        //why do I prefer this over the more complex solution (with less memory)
-        //because let's say more than 50% elements guaranteed to appear, faster runtime and more memory efficient.
-        //also compiler can optimise it better I suspect.
-
-
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int, pair<int, int>> countMap;
-        
-        for (int num : nums1) 
-        {
+
+        // Count frequencies in nums1
+        for (int num : nums1) {
             countMap[num].first++;
         }
-        
-        for (int num : nums2) 
-        {
+
+        // Count frequencies in nums2
+        for (int num : nums2) {
             countMap[num].second++;
         }
-        
+
         vector<int> result;
-        for (const auto& [num, counts] : countMap) 
-        {
-            int intersectionCount = min(counts.first, counts.second);
-            result.insert(result.end(), intersectionCount, num);
+        for (const auto &entry : countMap) {
+            
+            for (int i = 0; i < min(entry.second.first, entry.second.second); ++i) {
+                result.push_back(entry.first);
+            }
         }
-    
+
         return result;
     }
 };
