16bd9c02d0ba98cbb1e5b5b9722c69b43b1e227c
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-07-09 01:12:54 +0530

    Time: 8 ms (76.71%), Space: 16.4 MB (93.25%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,38 +1,20 @@
-#include <vector>
-#include <unordered_set>
-
 class Solution {
 public:
-    void setZeroes(std::vector<std::vector<int>>& matrix) {
-        std::unordered_set<int> row;
-        std::unordered_set<int> col;
-        
-        // Store the rows and columns that need to be set to zero
-        for (int i = 0; i < matrix.size(); ++i) {
-            for (int j = 0; j < matrix[0].size(); ++j) {
-                if (matrix[i][j] == 0) {
-                    row.emplace(i);
-                    col.emplace(j);
-                }
-            }
-        }
-        
-        // Set the respective rows to zero
-        for (const auto& r : row) {
-            for (int j = 0; j < matrix[0].size(); ++j) {
-                matrix[r][j] = 0;
-            }
-        }
-        
-        // Set the respective columns to zero
-        for (const auto& c : col) {
-            for (int i = 0; i < matrix.size(); ++i) {
-                matrix[i][c] = 0;
-            }
-        }
-        
-        // Clear the unordered sets to free up memory
-        row.clear();
-        col.clear();
+    void setZeroes(vector<vector<int>>& matrix) {
+    int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
+
+    for (int i = 0; i < rows; i++) {
+        if (matrix[i][0] == 0) col0 = 0;
+        for (int j = 1; j < cols; j++)
+            if (matrix[i][j] == 0)
+                matrix[i][0] = matrix[0][j] = 0;
     }
+
+    for (int i = rows - 1; i >= 0; i--) {
+        for (int j = cols - 1; j >= 1; j--)
+            if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                matrix[i][j] = 0;
+        if (col0 == 0) matrix[i][0] = 0;
+    }
+}
 };
