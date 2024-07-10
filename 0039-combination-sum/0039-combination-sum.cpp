5e38c409f4457051788519403358f7431b7f8497
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-07-11 04:01:27 +0530

    Time: 17 ms (32.43%), Space: 16 MB (39.77%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,41 +1,23 @@
-auto fastio = []() {
-    std::ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return nullptr;
-}();
-
 class Solution {
 public:
-    void com(vector<vector<int>>& res, const vector<int>& c, const int t, int sum, int i, vector<int> arr) {
-        // Base case
-        if (sum == t) {
-            res.push_back(arr);
-            return;
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+		// dp vector to store all possible combinations of the target sum
+        vector <vector <vector <int>>> dp(target+1);
+        dp[0]={{}};
+        for(int &i:nums)
+        {
+		// Iterating through all the elements from array nums aka candidates
+            for(int j=i;j<=target;j++)
+            {
+			// Finding all possible ways to achieve sum j from element i
+                for(auto v:dp[j-i])
+                {
+                    v.push_back(i);
+                    dp[j].push_back(v);
+                }
+            }
         }
-        if (i >= c.size() || sum > t) {
-            return;
-        }
-        
-        // Option 1: Do not include c[i]
-        com(res, c, t, sum, i + 1, arr);
-        
-        // Option 2: Include c[i] if it's valid
-        arr.push_back(c[i]);
-        com(res, c, t, sum + c[i], i, arr);
-        com(res, c, t, sum + c[i], i+1, arr);
-        
-    }
-
-    vector<vector<int>> combinationSum(vector<int>& c, int t) {
-        vector<vector<int>> res;
-        vector<int> arr;
-        com(res, c, t, 0, 0, arr); // Fixed vector initialization and function call
-  
-  // Remove duplicates
-        sort(res.begin(), res.end());
-        res.erase(unique(res.begin(), res.end()), res.end());
-
-        return res;
+		// Finally, returning our ans \U0001f643
+        return dp[target];
     }
 };
