645801c96bdf8751b5c9e4be10d91608eb11e64f
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-07-12 03:37:51 +0530

    Time: 0 ms (100%), Space: 11.5 MB (12.65%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    
-int findPeakElement(vector<int>& nums) {
-    int n = nums.size();
-    int left = 0, right = n - 1;
-    
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        
-        // Check if mid is a peak
-        if ((mid == 0 || nums[mid - 1] <= nums[mid]) &&
-            (mid == n - 1 || nums[mid + 1] <= nums[mid])) {
-            return mid;
+    int findPeakElement(vector<int>& v) {
+        int n=v.size()-1;
+
+        if(v.size()==1) return 0;
+        if(v[0]>v[1]) return 0;
+        if(v[n]>v[n-1]) return n;
+
+        int low=1, high=v.size()-2, ans=0;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(v[mid-1]<v[mid] && v[mid]>v[mid+1]){
+                ans=mid;
+                break;
+            }
+            if(v[mid-1]>v[mid]){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
         }
-        
-        // If there is a descending slope to the left, move right
-        if (mid > 0 && nums[mid - 1] > nums[mid]) {
-            right = mid - 1;
-        }
-        // If there is a descending slope to the right, move left
-        else {
-            left = mid + 1;
-        }
+        return ans;
     }
-    
-    return -1; // This should not happen as per the problem statement
-}
 };
