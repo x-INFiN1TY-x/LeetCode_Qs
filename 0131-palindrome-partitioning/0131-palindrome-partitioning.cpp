3625d43d34665c73084d1e0bfc87a3466a3ad0e8
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-07-12 00:10:49 +0530

    Time: 55 ms (99.17%), Space: 38.8 MB (100%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,33 +1,62 @@
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> pars;
-        vector<string> par;
-        partition(s, 0, par, pars);
-        return pars;
+    static vector<vector<string>> partition(string s) {
+        vector<vector<string>> res;
+        vector<string> tmp;
+        dfs(0, tmp, s, res);
+        return res;
     }
-private: 
-    void partition(string& s, int start, vector<string>& par, vector<vector<string>>& pars) {
-        int n = s.length();
-        if (start == n) {
-            pars.push_back(par);
-        } else {
-            for (int i = start; i < n; i++) {
-                if (isPalindrome(s, start, i)) {
-                    par.push_back(s.substr(start, i - start + 1));
-                    partition(s, i + 1, par, pars);
-                    par.pop_back();
-                }
-            }
-        }
-    }
-    
-    bool isPalindrome(string& s, int l, int r) {
-        while (l < r) {
-            if (s[l++] != s[r--]) {
-                return false;
-            }
-        }
+
+    static constexpr bool isPalindrome(const string& str) {
+        for (int l = 0, r = str.size() - 1; l < r; ++l, --r)
+            if (str[l] != str[r]) return false;
         return true;
     }
+
+    static void dfs(int i, vector<string>& cur, const string& s, vector<vector<string>>& res) {
+        if (i == s.size()) {
+            res.push_back(cur);
+            return;
+        }
+        for (int start = i; i < s.size(); ++i) {
+            cur.push_back(s.substr(start, i - start + 1));
+            if (isPalindrome(cur.back()))
+                dfs(i + 1, cur, s, res);
+            cur.pop_back();
+        }
+    }
 };
+
+int init = [] {
+    ofstream out("user.out");
+    cout.rdbuf(out.rdbuf());
+    for (string str; getline(cin, str); cout << '\n') {
+        vector<vector<string>> res = Solution::partition(str.substr(1, str.size() - 2));
+        cout << '[';
+        bool a = true;
+        for (const auto& v : res) {
+            if (a) a = false;
+            else cout << ',';
+            cout << '[';
+            bool b = true;
+            for (const auto& s : v) {
+                if (b) b = false;
+                else cout << ',';
+                cout << '\"' << s << '\"';
+
+            }
+            cout << ']';
+
+        }
+        cout << ']';
+    }
+    exit(0);
+    return 0;
+}();
