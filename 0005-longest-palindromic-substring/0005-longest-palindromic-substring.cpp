186dc7b5acf01dd8cc70221624a8684f2514c859
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-07-16 01:23:09 +0530

    Time: 106 ms (45.66%), Space: 59.5 MB (30.13%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -5,25 +5,34 @@ public:
             return s;
         }
         
-        int max_len = 1;
-        int start = 0;
-        int end = 0;
-        std::vector<std::vector<bool>> dp(s.length(), std::vector<bool>(s.length(), false));
+        int maxLen = 1;
+        std::string maxStr = s.substr(0, 1);
+        s = "#" + std::regex_replace(s, std::regex(""), "#") + "#";
+        std::vector<int> dp(s.length(), 0);
+        int center = 0;
+        int right = 0;
         
         for (int i = 0; i < s.length(); ++i) {
-            dp[i][i] = true;
-            for (int j = 0; j < i; ++j) {
-                if (s[j] == s[i] && (i - j <= 2 || dp[j + 1][i - 1])) {
-                    dp[j][i] = true;
-                    if (i - j + 1 > max_len) {
-                        max_len = i - j + 1;
-                        start = j;
-                        end = i;
-                    }
-                }
+            if (i < right) {
+                dp[i] = std::min(right - i, dp[2 * center - i]);
+            }
+            
+            while (i - dp[i] - 1 >= 0 && i + dp[i] + 1 < s.length() && s[i - dp[i] - 1] == s[i + dp[i] + 1]) {
+                dp[i]++;
+            }
+            
+            if (i + dp[i] > right) {
+                center = i;
+                right = i + dp[i];
+            }
+            
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                maxStr = s.substr(i - dp[i], 2 * dp[i] + 1);
+                maxStr.erase(std::remove(maxStr.begin(), maxStr.end(), '#'), maxStr.end());
             }
         }
         
-        return s.substr(start, end - start + 1);
+        return maxStr;
     }
 };
