5353603d44c9564c65062d4adf353ea5d69f2e04
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-07-16 01:23:15 +0530

    Time: 14 ms (75.4%), Space: 18.4 MB (54.82%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -4,35 +4,29 @@ public:
         if (s.length() <= 1) {
             return s;
         }
-        
-        int maxLen = 1;
-        std::string maxStr = s.substr(0, 1);
-        s = "#" + std::regex_replace(s, std::regex(""), "#") + "#";
-        std::vector<int> dp(s.length(), 0);
-        int center = 0;
-        int right = 0;
-        
-        for (int i = 0; i < s.length(); ++i) {
-            if (i < right) {
-                dp[i] = std::min(right - i, dp[2 * center - i]);
+
+        auto expand_from_center = [&](int left, int right) {
+            while (left >= 0 && right < s.length() && s[left] == s[right]) {
+                left--;
+                right++;
             }
-            
-            while (i - dp[i] - 1 >= 0 && i + dp[i] + 1 < s.length() && s[i - dp[i] - 1] == s[i + dp[i] + 1]) {
-                dp[i]++;
+            return s.substr(left + 1, right - left - 1);
+        };
+
+        std::string max_str = s.substr(0, 1);
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            std::string odd = expand_from_center(i, i);
+            std::string even = expand_from_center(i, i + 1);
+
+            if (odd.length() > max_str.length()) {
+                max_str = odd;
             }
-            
-            if (i + dp[i] > right) {
-                center = i;
-                right = i + dp[i];
-            }
-            
-            if (dp[i] > maxLen) {
-                maxLen = dp[i];
-                maxStr = s.substr(i - dp[i], 2 * dp[i] + 1);
-                maxStr.erase(std::remove(maxStr.begin(), maxStr.end(), '#'), maxStr.end());
+            if (even.length() > max_str.length()) {
+                max_str = even;
             }
         }
-        
-        return maxStr;
+
+        return max_str;
     }
 };
