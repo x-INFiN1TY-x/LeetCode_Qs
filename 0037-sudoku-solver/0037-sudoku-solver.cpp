73c0495991108cbb7a6d1af1832b7d083597dafe
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-07-21 04:48:17 +0530

    Time: 14 ms (45.46%), Space: 8.1 MB (18.73%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,41 +1,19 @@
-/*
-
-    Time Complexity : O(9^(N*N)) : For every unassigned index there are 9 possible options so the worst-case time
-    complexity of sudoku solver is O(9^(N*N)).
-
-    Space Complexity : O(1), Constant space.
-
-    Solved using Matrix + Backtracking.
-
-*/
-
+//Solution 01:
 class Solution {
-private:
-    bool isValid(vector<vector<char>>& board, int row, int col, char ch){
-        for(int i=0; i<9; i++){
-            if(board[i][col] == ch){
-                return false;
-            }
-            if(board[row][i] == ch){
-                return false;
-            }
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3] == ch){
-                return false;
-            }
-        }
-        return true;
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
     }
-    bool solve(vector<vector<char>>& board) {
+    
+    bool solve(vector<vector<char>>& board){
         for(int i=0; i<board.size(); i++){
             for(int j=0; j<board[0].size(); j++){
                 if(board[i][j] == '.'){
-                    for(char ch='1'; ch<='9'; ch++){
-                        if(isValid(board, i, j, ch)){
-                            board[i][j] = ch;
-                            if(solve(board) == true){
-                                return true;
-                            }
-                            board[i][j] = '.';
+                    for(char c='1'; c<='9'; c++){
+                        if(valid(c, board, i, j)){
+                            board[i][j] = c;
+                            if(solve(board) == true) return true;
+                            else board[i][j]='.';
                         }
                     }
                     return false;
@@ -44,8 +22,13 @@ private:
         }
         return true;
     }
-public:
-    void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+    
+    bool valid(char c, vector<vector<char>>& board, int row, int col){
+        for(int i=0; i<9; i++){
+            if(board[i][col] == c) return false;
+            if(board[row][i] == c) return false;
+            if(board[3*(row/3)+(i/3)][3*(col/3)+(i%3)] == c) return false;
+        }
+        return true;
     }
 };
