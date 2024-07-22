cedbe29fc2853966f39212710c9a42362d63491e
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-07-23 00:54:36 +0530

    Time: 3 ms (58.70%) | Memory: 12.4 MB (30.30%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,26 +1,21 @@
 class Solution {
-private:
-    int helper(int ind, vector<int> &days, vector<int> &costs, vector<int> &dp) {
-        if(ind >= days.size()) {
-            return 0;
-        }
-        if(dp[ind] != -1) return dp[ind];
-        int pass1 = costs[0] + helper(ind+1, days, costs, dp);
-
-        int cur7 = days[ind] + 6;
-        int nextIndex4_7 = upper_bound(days.begin(), days.end(), cur7) - days.begin();
-        int pass2 = costs[1]+helper(nextIndex4_7, days, costs, dp);
-
-        int cur30 = days[ind] + 29;
-        int nextIndex4_30 = upper_bound(days.begin(), days.end(), cur30) - days.begin();
-        int pass3 = costs[2]+helper(nextIndex4_30, days, costs, dp);
-
-        return dp[ind] = min(pass1, min(pass2, pass3));
-    }
 public:
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int count = 0;
-        vector<int> dp(days.size(), -1);
-        return helper(0, days, costs, dp);
+    int mincostTickets(vector<int>& DAYS, vector<int>& cost) {
+        
+        unordered_set<int> days(DAYS.begin(),DAYS.end());
+        vector<int> dp(367);
+        
+        for(int i=1 ; i<366 ; i++)
+        {
+            dp[i] = dp[i-1];
+            if(days.find(i) != days.end())
+            {
+			// cost of ith day , decision of ith day will be
+                dp[i] = min({ dp[i - 1] + cost[0],   // 1st case
+                             dp[max(0, i - 7)] + cost[1],  // 2nd case
+                             dp[max(0, i - 30)] + cost[2]});  // 3rd case
+            }
+        }
+        return dp[365];
     }
 };
