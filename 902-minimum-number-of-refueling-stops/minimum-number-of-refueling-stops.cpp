8a62d15bfa145202f24598792f10a2c8f8221386
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-07-23 04:07:01 +0530

    Time: 96 ms (5.02%) | Memory: 22.6 MB (7.11%) - LeetSync

--- 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
@@ -1,30 +1,41 @@
-#include <vector>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
-    int minRefuelStops(int target, int kms, vector<vector<int>>& stations) {
-        if(kms >= target) return 0;
-        if(stations.empty() || kms < stations[0][0]) return -1;
+    
+    long int dp[555][555];
+    
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        stations.insert(stations.begin(), {0, startFuel});
+        stations.push_back({target, 0});
+        int n = stations.size();
+        memset(dp, -1, sizeof dp);
         
-        int i = 0, stops = 0;
-        priority_queue<int> mheap;
+        dp[0][0] = 0;
         
-        while(kms < target) {
-            while(i < stations.size() && stations[i][0] <= kms) {
-                mheap.push(stations[i][1]);
-                i++;
+        // n-1 since we dont need to refuel on target we just need the answer on it.
+        for(int i=0;i<n-1;i++)
+            for(int j=0;j<n-1;j++)
+            {
+                if(dp[i][j] >= 0)
+                {
+                    // without petrol
+                    if(dp[i][j] >= stations[i+1][0] - stations[i][0])
+                        dp[i+1][j] = max(dp[i+1][j], dp[i][j] - stations[i+1][0] + stations[i][0]);
+                    
+                    // with petrol
+                    if(dp[i][j] + stations[i][1] >= stations[i+1][0] - stations[i][0])
+                        dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j] + stations[i][1]- stations[i+1][0] + stations[i][0]);    
+                }
             }
-            
-            if(mheap.empty()) return -1;  // If no stations are reachable and we need fuel
-            
-            kms += mheap.top();
-            mheap.pop();
-            stops++;
-        }
         
-        return stops;
+        int ans = 0;
+        // n-1th index is the target
+        for(int j=0;j<n;j++)
+            if(dp[n-1][j] >= 0)
+            {
+                ans = j;
+                break;
+            }
+        // subtratcing -1 since for stating we are adding + 1 for taking starting fuel.
+        return ans-1;        
     }
 };
