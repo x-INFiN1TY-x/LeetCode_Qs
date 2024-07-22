059c596b955636612832917b40ab02450b1a0a6a
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-07-23 04:07:29 +0530

    Time: 21 ms (57.74%) | Memory: 20 MB (43.24%) - LeetSync

--- 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
@@ -1,41 +1,50 @@
 class Solution {
 public:
-    
-    long int dp[555][555];
-    
-    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
-        stations.insert(stations.begin(), {0, startFuel});
-        stations.push_back({target, 0});
+    int minRefuelStops(int target, int start_fuel, vector<vector<int>>& stations) {
+        
         int n = stations.size();
-        memset(dp, -1, sizeof dp);
         
-        dp[0][0] = 0;
+        // declare a max. heap
         
-        // n-1 since we dont need to refuel on target we just need the answer on it.
-        for(int i=0;i<n-1;i++)
-            for(int j=0;j<n-1;j++)
+        priority_queue<int> pq;
+        
+        int curr_dist = start_fuel;
+        
+        // count will store the no. of stops required
+        
+        int count = 0;
+        
+        int i = 0;
+        
+        // run the loop until curr_dist < target
+        
+        while(curr_dist < target)
+        {
+            // push the fuel into pq. until farthest position we can reach
+            
+            while(i < n && stations[i][0] <= curr_dist)
             {
-                if(dp[i][j] >= 0)
-                {
-                    // without petrol
-                    if(dp[i][j] >= stations[i+1][0] - stations[i][0])
-                        dp[i+1][j] = max(dp[i+1][j], dp[i][j] - stations[i+1][0] + stations[i][0]);
-                    
-                    // with petrol
-                    if(dp[i][j] + stations[i][1] >= stations[i+1][0] - stations[i][0])
-                        dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j] + stations[i][1]- stations[i+1][0] + stations[i][0]);    
-                }
+                pq.push(stations[i][1]);
+                
+                i++;
             }
+            
+            // if pq is empty, then it is not possible to reach the target, return -1
+            
+            if(pq.empty())
+                return -1;
+            
+            // add the max. fuel from pq
+            
+            curr_dist += pq.top();
+            
+            pq.pop();
+            
+            // increment the no. of stops
+            
+            count++;
+        }
         
-        int ans = 0;
-        // n-1th index is the target
-        for(int j=0;j<n;j++)
-            if(dp[n-1][j] >= 0)
-            {
-                ans = j;
-                break;
-            }
-        // subtratcing -1 since for stating we are adding + 1 for taking starting fuel.
-        return ans-1;        
+        return count;
     }
 };
