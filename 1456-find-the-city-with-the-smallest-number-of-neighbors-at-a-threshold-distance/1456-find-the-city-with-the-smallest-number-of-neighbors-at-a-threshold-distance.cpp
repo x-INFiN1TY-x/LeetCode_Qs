c92fab2eda7942a970280e30e4d0fb798d2ada23
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-07-27 01:19:42 +0530

    Time: 23 ms (80.76%), Space: 15.4 MB (79.93%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> distance(n, vector<int>(n, 1000000000)); // Initialize distance matrix with infinity
-
-        for (int i = 0; i < n; ++i) {
-            distance[i][i] = 0; // Distance to itself is 0
+        vector<vector<int>>matrix(n,vector<int>(n,1e9));
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            int w=edges[i][2];
+            matrix[u][v]=w;
+            matrix[v][u]=w;
         }
-
-        for (auto& edge : edges) {
-            distance[edge[0]][edge[1]] = edge[2];
-            distance[edge[1]][edge[0]] = edge[2];
+        for(int i=0;i<n;i++){
+            matrix[i][i]=0;
         }
-
-        // Floyd-Warshall algorithm to find all-pairs shortest path
-        for (int k = 0; k < n; ++k) {
-            for (int i = 0; i < n; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j]);
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    matrix[i][j]=min(matrix[i][j],matrix[i][k]+matrix[k][j]);
                 }
             }
         }
-
-        int ans = -1;
-        int mini = INT_MAX;
-        for (int i = 0; i < n; ++i) {
-            int count = 0;
-            for (int j = 0; j < n; ++j) {
-                if (i != j && distance[i][j] <= distanceThreshold) {
-                    ++count;
-                }
+        vector<int>dist(n,0);
+        for(int i=0;i<n;i++){
+            int c=0;
+            for(int j=0;j<n;j++){
+                 if(matrix[i][j]<=distanceThreshold){
+                    c++;
+                 }
             }
-            if (count <= mini) {
-                mini = count;
-                ans = i;
-            }
+            dist[i]=c;
         }
-
-        return ans;
+        int min=dist[0],node=0;
+        for(int i=1;i<n;i++){
+          if(dist[i]<=min){
+            min=dist[i];
+            node=i;
+          }
+        }
+        return node;
     }
 };
