596b502b931f97edc996ecee09f16e0501e021b7
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-07-28 05:19:33 +0530

    Time: 3 ms (67.89%) | Memory: 7.5 MB (52.43%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int minSteps(int n) {
-        if(n == 1) return 0;
-        int count = 0;
-        bool *isPrime = new bool[n + 1];
-        for(int i = 2; i < n + 1; i++) isPrime[i] = true;
-        for(int i = 2; i < n + 1; i++)
-        {
-            if(isPrime[i] == true)
-            {
-                while(n % i == 0)
-                {
-                    count += i;
-                    n /= i;
-                }
-                for(int j = i * i; j < n + 1; j += i) 
-                {
-                    if(isPrime[j]) isPrime[j] = false;
+        if (n == 1)
+            return 0;
+        
+        int min = 100000;
+        int minQuotient= 1;
+        int currentValue = n;
+        int result = 0;
+        
+        while (currentValue != 1) {
+            for (int j = 1; j <= n; ++j) {
+                if (currentValue % j == 0) {
+                    auto tmp = currentValue / j;
+                    if (tmp != 1) {
+                        min = std::min(min, tmp);
+                        minQuotient = j;
+                    }
                 }
             }
+            result += min;
+            currentValue = minQuotient;
+            min = 1000;
         }
-        return count;
+
+        return result;
     }
 };
