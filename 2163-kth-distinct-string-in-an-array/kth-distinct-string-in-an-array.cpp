4f6e314264f9a43f766e0b7aa609ae8774b6df59
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-08-06 00:59:27 +0530

    Time: 10 ms (93.63%) | Memory: 18.5 MB (20.16%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
@@ -1,46 +1,24 @@
-// struct Trie for N alphabets
-const int N=26;
-struct Trie {
-    Trie* next[N];
-    int count=0;
-
-    Trie() {
-    //    cout<<"Create a trie object!\n";
-        memset(next, 0, sizeof(next));
-    }
-
-    void insert(string& s) {
-        Trie* Node=this;
-        for(char c: s){
-            int i=c-'a';
-            if (Node->next[i]==NULL) Node->next[i]=new Trie();
-            Node=Node->next[i];
-        }
-        Node->count++;
-    }
-    bool findcnt1(string& s) {
-        Trie* Node=this;
-        for(char c: s){
-            int i=c-'a';
-            if (Node->next[i]==NULL) return 0;
-            Node=Node->next[i];
-        }
-        return Node->count==1;
-    }
-};
-
-
 class Solution {
 public:
-    static string kthDistinct(vector<string>& arr, int k) {
-        Trie trie;
+    string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string, int> mp;
+        mp.reserve(arr.size());
         for(string& s: arr)
-            trie.insert(s);
+            mp[s]++;
 
         for(string& s: arr){
-            if (trie.findcnt1(s)==1) k--;
+            if (mp[s]==1) k--;
             if (k==0)  return s;
         }
         return "";  
     }
 };
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
