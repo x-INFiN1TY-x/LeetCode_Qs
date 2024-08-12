043b9a33c6671b36dd7a48a1604bf2bbd27e2819
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-08-12 16:05:25 +0530

    Time: 6 ms (28.40%) | Memory: 11.3 MB (56.40%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,12 +1,20 @@
-class Solution {
-public:
-    bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int, int> mp;
-        for (int& i : arr)
-            ++mp[i];
-        unordered_set<int> s;
-        for (auto& i : mp)
-            s.insert(i.second);
-        return mp.size() == s.size();
-    }
+class Solution
+{
+    public:
+        bool uniqueOccurrences(vector<int> & arr)
+        {
+            sort(begin(arr), end(arr));
+            vector ans(1001, 0);
+            int i = 0;
+            while (i < arr.size())
+            {
+                int count = 1;
+                int j = i + 1;
+                while (j < arr.size() and arr[i] == arr[j]) count++, j++;
+                if (ans[count]) return 0;
+                ans[count] = 1;
+                i = j;
+            }
+            return 1;
+        }
 };
