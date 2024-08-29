d7a37701623c9cc5bf0b4328842fa0ed4eb19d47
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-08-29 22:27:33 +0530

    Time: 26 ms (5.55%) | Memory: 16.1 MB (5.33%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,15 +1,15 @@
 class Solution {
+    void dfs(vector<vector<int>>& rooms, unordered_set<int> & keys, unordered_set<int> & visited, int curr) {
+        visited.insert(curr);
+        for (int k : rooms[curr]) keys.insert(k);
+        for (int k : keys) if (visited.find(k) == visited.end()) dfs(rooms, keys, visited, k);
+    }
+    
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        unordered_set<int> keys;
         unordered_set<int> visited;
-        queue<int> to_visit;
-        to_visit.push(0);
-        while(!to_visit.empty()) {
-            int curr = to_visit.front();
-            to_visit.pop();
-            visited.insert(curr);
-            for (int k : rooms[curr]) if (visited.find(k) == visited.end()) to_visit.push(k);
-        }
+        dfs(rooms, keys, visited, 0);
         return visited.size() == rooms.size();
     }
 };
