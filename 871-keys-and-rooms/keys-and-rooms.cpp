d2a1e06e715ce3285614e36e69b531095a4a98ec
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-08-29 22:25:06 +0530

    Time: 7 ms (65.85%) | Memory: 14.3 MB (32.17%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,31 +1,39 @@
-auto fastio=[]()  {
-    std::ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return nullptr;
-}();
+#include <vector>
+using namespace std;
 
-class Solution {
-public:
-    void dfs(vector<vector<int>>& rooms, int node, vector<int>& visited) {
-        visited[node] = 1; // Mark room as visited
-        for (int i : rooms[node]) {
-            if (!visited[i]) {
-                dfs(rooms, i, visited);
+class Solution
+{
+    public:
+        void dfs(vector<bool> &vis, vector<vector< int>> &rooms, int room)
+        {
+            vis[room] = true;	// Mark the current room as visited
+            for (int key: rooms[room])
+            {
+            	// Iterate through all keys in the current room
+                if (!vis[key])
+                {
+                	// If the room corresponding to the key hasn't been visited
+                    dfs(vis, rooms, key);	// Perform DFS for that room
+                }
             }
         }
-    }
 
-    bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        int n = rooms.size();
-        vector<int> visited(n, 0); // Initialize all rooms as not visited
-        dfs(rooms, 0, visited); // Start DFS from room 0
-        // Check if all rooms have been visited during DFS
-        for (int v : visited) {
-            if (v == 0) {
-                return false; // If any room is not visited, return false
+    bool canVisitAllRooms(vector<vector < int>> &rooms)
+    {
+        int n = rooms.size();	// Number of rooms
+        vector<bool> vis(n, false);	// Visited rooms tracker
+
+        dfs(vis, rooms, 0);	// Start DFS from room 0
+
+        for (bool visited: vis)
+        {
+            if (!visited)
+            {
+            	// If any room hasn't been visited, return false
+                return false;
             }
         }
-        return true; // All rooms have been visited
+
+        return true;	// If all rooms have been visited, return true
     }
 };
