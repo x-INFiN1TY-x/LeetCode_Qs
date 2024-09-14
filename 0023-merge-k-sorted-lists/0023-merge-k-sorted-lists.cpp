436b105289df575e120fd3a1027a47e07fe05b28
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-09-14 17:40:37 +0530

    Time: 10 ms (94.98%), Space: 19 MB (19.41%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,55 +1,33 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    
-    ListNode* mergesort(ListNode* a  , ListNode* b) {
-        if(!a) 
-            return b;
-        if(!b)
-            return a;
-        ListNode* ans=NULL;
-        if(a->val<=b->val)
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0) return NULL;
+        priority_queue<int,vector<int>,greater<int>> pq;
+        
+        for(int i=0;i<lists.size();i++)
         {
-            ans=a;
-            ans->next = mergesort(a->next,b);
-        }
-        else
-        {
-            ans=b;
-            ans->next = mergesort(a,b->next);
-        }
-        return ans;
-    }
-    
-    ListNode* mergeK(vector<ListNode*>& lists,int k) {
-        while(k!=0) {
-            int i = 0,j = k;
-            while(i<j) {
-                lists[i]=mergesort(lists[i],lists[j]);
-                i++;
-                j--;
-                if(i>=j) 
-                    k=j;
+            ListNode* head = lists[i];
+            while(head!=NULL)
+            {
+                pq.push(head->val);
+                head = head->next;
             }
         }
-        return lists[0];
-    }
-    
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size()==1)
-            return lists[0];
-        if(lists.size()==0) 
-            return NULL;
-        ListNode*newhead = mergeK(lists,lists.size()-1);
-        return newhead;
+        ListNode* start = NULL;
+        ListNode* end = NULL;
+        while(!pq.empty()) {
+            if(start==NULL)
+            {
+                start = new ListNode(pq.top());
+                end = start;
+                pq.pop();
+            }
+            else{
+                end->next = new ListNode(pq.top());
+                pq.pop();
+                end = end->next;
+            }
+        }
+            return start;
     }
 };
