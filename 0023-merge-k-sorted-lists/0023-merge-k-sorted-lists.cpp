1927c19c3998d17ed2a0d40c5438bc907acdec3b
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-09-20 19:04:16 +0530

    Time: 16 ms (64.64%), Space: 18.7 MB (35.18%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) {
-            return nullptr;
+        if(lists.size() == 0) return NULL;
+
+        ListNode* dummyHead = new ListNode(-1);
+        ListNode* dummyTail = dummyHead;
+
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> pq;
+        for(auto head : lists)  if(head != NULL) pq.push({head->val, head});
+
+        while(!pq.empty()){
+            ListNode* minNode = pq.top().second;
+            pq.pop();
+            if(minNode->next != NULL) pq.push({minNode->next->val, minNode->next});
+
+            dummyTail->next = minNode;
+            dummyTail = dummyTail->next;
         }
-        return mergeKListsHelper(lists, 0, lists.size() - 1);
-    }
-    
-    ListNode* mergeKListsHelper(vector<ListNode*>& lists, int start, int end) {
-        if (start == end) {
-            return lists[start];
-        }
-        if (start + 1 == end) {
-            return merge(lists[start], lists[end]);
-        }
-        int mid = start + (end - start) / 2;
-        ListNode* left = mergeKListsHelper(lists, start, mid);
-        ListNode* right = mergeKListsHelper(lists, mid + 1, end);
-        return merge(left, right);
-    }
-    
-    ListNode* merge(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
-        
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-                curr->next = l1;
-                l1 = l1->next;
-            } else {
-                curr->next = l2;
-                l2 = l2->next;
-            }
-            curr = curr->next;
-        }
-        
-        curr->next = l1 ? l1 : l2;
-        
-        return dummy->next;
+        return dummyHead->next;
     }
 };
